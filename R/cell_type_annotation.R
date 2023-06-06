9af9e91b63461899b10e82c301f0e108451cf041
Zhen-Miao <39448538+Zhen-Miao@users.noreply.github.com>
2023-06-05 21:45:18 -0400

    updated functions

---
+++ R/cell_type_annotation.R
@@ -0,0 +1,143 @@
+
+estimate_label_no_cap_rate <- function(
+    r_by_t,
+    in_r_by_c,
+    alpha = 1
+){
+  ## build prediction matrix
+  esti_m5 = matrix(nrow = dim(in_r_by_c)[2], ncol = dim(r_by_t)[2]) ## row cell, column cell types
+  rownames(esti_m5) <- colnames(in_r_by_c)
+  colnames(esti_m5) <- colnames(r_by_t)
+
+  if(class(in_r_by_c) == 'dgCMatrix'){
+    if(any(in_r_by_c@x != 0 &  in_r_by_c@x != 1)){
+      stop("the matrix should be a binary matrix!")
+    }
+  }else{
+    if(any(in_r_by_c != 0 & in_r_by_c != 1)){
+      stop("the matrix should be a binary matrix!")
+    }
+  }
+  n_reads <- Matrix::colSums(in_r_by_c)
+  sum_prob <- colSums(r_by_t)
+  capturing_rate_mat <- t(outer(n_reads,sum_prob, "/"))
+  capturing_rate_mat[capturing_rate_mat > 0.9995] <- 0.9995
+  capturing_rate_mat[capturing_rate_mat < 0.00005] <- 0.00005
+
+
+  if(as.numeric(dim(in_r_by_c)[1]) * as.numeric(dim(in_r_by_c)[2]) > (2^31-1)){
+
+    ## need to split
+    split_by = ceiling(   (as.numeric(dim(in_r_by_c)[1]) * as.numeric(dim(in_r_by_c)[2]) ) / (2^31-1) )
+    dim2 = ceiling(dim(in_r_by_c)[2] / split_by)
+    for(i in c(1:split_by)){
+      in_r_by_c_mat = as.matrix(in_r_by_c[,((i-1)*dim2+1):(min(i*dim2,dim(in_r_by_c)[2])) ])
+      # in_capturing_rate = capturing_rate[((i-1)*dim2+1):(min(i*dim2,dim(in_r_by_c)[2]))]
+
+      ## predict cell types
+      for(i_cell in 1:dim(in_r_by_c_mat)[2]){
+        pqbyt <- r_by_t * capturing_rate_mat[,i_cell+ (i-1)*dim2 ]
+        lg_pqbyt = log( pqbyt   )
+        lg_pqbyt_q = log1p( -1 * pqbyt)
+        # unparallelized
+        xvec <- in_r_by_c_mat[,i_cell]
+        esti_m5[i_cell + (i-1)*dim2 ,] = Rfast::colsums(lg_pqbyt * xvec + alpha * lg_pqbyt_q * (1-xvec))
+      }
+      saveRDS(esti_m5,'esti_m5_intermediate_ave_high q_12.rds')
+    }
+  }else{
+    in_r_by_c_mat = as.matrix(in_r_by_c)
+
+    ## predict cell types
+
+    # unparallelized
+    for(i_cell in 1:dim(esti_m5)[1]){
+      pqbyt <- r_by_t * capturing_rate_mat[,i_cell]
+      lg_pqbyt = log( pqbyt   )
+      lg_pqbyt_q = log1p( -1 * pqbyt)
+      # unparallelized
+      xvec <- in_r_by_c_mat[,i_cell]
+      esti_m5[i_cell,] = Rfast::colsums(lg_pqbyt * xvec + lg_pqbyt_q * (1-xvec))
+
+      # esti_m5[i_cell,] = apply(lg_pqbyt, 2, function(x) sum(x[in_r_by_c_mat[,i_cell] > 0])) +
+      #   alpha*  apply(lg_pqbyt_q, 2, function(x) sum(x[in_r_by_c_mat[,i_cell] == 0]))
+    }
+    return(esti_m5)
+  }
+}
+
+estimate_label_no_cap_rate_all_pks <- function(
+    r_by_t,
+    in_r_by_c,
+    # capturing_rate,
+    pks_sel, ## selected peaks as informative for cell type label prediction
+    alpha = 1
+){
+  ## build prediction matrix
+  esti_m5 = matrix(nrow = dim(in_r_by_c)[2], ncol = dim(r_by_t)[2]) ## row cell, column cell types
+  rownames(esti_m5) <- colnames(in_r_by_c)
+  colnames(esti_m5) <- colnames(r_by_t)
+
+  if(class(in_r_by_c) == 'dgCMatrix'){
+    if(any(in_r_by_c@x != 0 &  in_r_by_c@x != 1)){
+      stop("the matrix should be a binary matrix!")
+    }
+  }else{
+    if(any(in_r_by_c != 0 & in_r_by_c != 1)){
+      stop("the matrix should be a binary matrix!")
+    }
+  }
+  n_reads <- Matrix::colSums(in_r_by_c)
+  sum_prob <- colSums(r_by_t)
+  capturing_rate_mat <- t(outer(n_reads,sum_prob, "/"))
+  capturing_rate_mat[capturing_rate_mat > 0.9995] <- 0.9995
+  capturing_rate_mat[capturing_rate_mat < 0.00005] <- 0.00005
+
+  ## filter peaks to keep only relevant ones
+  r_by_t = r_by_t[pks_sel,]
+  in_r_by_c = in_r_by_c[pks_sel,]
+
+
+  if(as.numeric(dim(in_r_by_c)[1]) * as.numeric(dim(in_r_by_c)[2]) > (2^31-1)){
+
+    ## need to split
+    split_by = ceiling(   (as.numeric(dim(in_r_by_c)[1]) * as.numeric(dim(in_r_by_c)[2]) ) / (2^31-1) )
+    dim2 = ceiling(dim(in_r_by_c)[2] / split_by)
+    for(i in c(1:split_by)){
+      in_r_by_c_mat = as.matrix(in_r_by_c[,((i-1)*dim2+1):(min(i*dim2,dim(in_r_by_c)[2])) ])
+      # in_capturing_rate = capturing_rate[((i-1)*dim2+1):(min(i*dim2,dim(in_r_by_c)[2]))]
+
+      ## predict cell types
+      for(i_cell in 1:dim(in_r_by_c_mat)[2]){
+        pqbyt <- r_by_t * capturing_rate_mat[,i_cell+ (i-1)*dim2 ]
+        lg_pqbyt = log( pqbyt   )
+        lg_pqbyt_q = log1p( -1 * pqbyt)
+        # unparallelized
+        xvec <- in_r_by_c_mat[,i_cell]
+        esti_m5[i_cell + (i-1)*dim2 ,] = Rfast::colsums(lg_pqbyt * xvec + alpha * lg_pqbyt_q * (1-xvec))
+      }
+      saveRDS(esti_m5,'esti_m5_intermediate_ave_high q_12.rds')
+    }
+  }else{
+    in_r_by_c_mat = as.matrix(in_r_by_c)
+
+    ## predict cell types
+
+    # unparallelized
+    for(i_cell in 1:dim(esti_m5)[1]){
+      pqbyt <- r_by_t * capturing_rate_mat[,i_cell]
+      lg_pqbyt = log( pqbyt   )
+      lg_pqbyt_q = log1p( -1 * pqbyt)
+      # unparallelized
+      xvec <- in_r_by_c_mat[,i_cell]
+      esti_m5[i_cell,] = Rfast::colsums(lg_pqbyt * xvec + lg_pqbyt_q * (1-xvec))
+
+      # esti_m5[i_cell,] = apply(lg_pqbyt, 2, function(x) sum(x[in_r_by_c_mat[,i_cell] > 0])) +
+      #   alpha*  apply(lg_pqbyt_q, 2, function(x) sum(x[in_r_by_c_mat[,i_cell] == 0]))
+    }
+    return(esti_m5)
+  }
+}
+
+
+
--- R/param_estimate_joint_p_q.R
+++ R/param_estimate_joint_p_q.R
@@ -1,9 +1,25 @@
 ## param_estimate_joint_p_q
 
 
-#' Return
+#' Get region (peak) by cell type matrix and per cell capturing rate
+#' @description For a snATAC-seq (binary) dataset, compute the peak-specific open probability
+#'  and cell-specific capturing rates
 #'
+#' @importFrom methods is
+#' @param cell_type_set A vector containing all cell types
+#' @param r_by_c Input matrix, region (peak) by cell
+#' @param cell_type_labels A vector containing cell type labels
+#' @param n_features_per_cell The number of features in the matrix, can be calculated by nrow(r_by_c)
+#' @param p_acc The accuracy of p, default specified as 0.0005
+#' @param q_acc The accuracy of q, default specified as 0.0005
+#' @param n_max_iter The maximum iteration, default = 800
+#' @param verbose Whether to output information on processing status
 #'
+#' @return A list with two elements, \itemize{
+#'   \item p_by_t Peak by cell type matrix, each element represents the open probability of the peak in the corresponding cell type
+#'   \item q_vec A vector of cell-specific capturing rate
+#' }
+#' @export
 #'
 get_r_by_ct_mat_pq <- function(cell_type_set,
                                r_by_c,
@@ -11,79 +27,35 @@ get_r_by_ct_mat_pq <- function(cell_type_set,
                                n_features_per_cell,
                                p_acc = 0.0005,
                                q_acc = 0.0005,
-                               n_max_iter = 300) {
-
-  ## require r_by_c to have column names
+                               n_max_iter = 800,
+                               verbose = T) {
+  ## require cell names provided
   if (is.null(colnames(r_by_c))) {
     stop("the peak by cell matrix has to have column names")
   }
-  ## save arguments
-  ## note, with this method, there cannot be any unused argument,
-  ## or error will happen
-  args_s <- list()
-  args_s <- append(args_s, mget(names(formals()), sys.frame(sys.nframe())))
 
-  ## get output with internal functions
-  outs <- do.call(get_r_by_ct_mat_pq_int, args = args_s)
-  itermat_p_by_type <- outs$itermat_p_by_type
-  itermat_q_by_type <- outs$itermat_q_by_type
-  print(str(itermat_p_by_type))
+  ## save data to matrix
+  itermat_q_by_type <- vector(length = dim(r_by_c)[2])
+  names(itermat_q_by_type) <- colnames(r_by_c)
+  itermat_p_by_type <- matrix(nrow = n_features_per_cell, ncol = length(cell_type_set))
+  colnames(itermat_p_by_type) <- cell_type_set
 
-  ## remove total na values
-  for (gg in cell_type_set) {
-    mat_to_shrink <- itermat_p_by_type[[gg]]
-    mat_to_shrink <- mat_to_shrink[, !is.na(colsums(mat_to_shrink))]
-    itermat_p_by_type[[gg]] <- mat_to_shrink
-
-    mat_to_shrink2 <- itermat_q_by_type[[gg]]
-    mat_to_shrink2 <- mat_to_shrink2[!is.na(rowsums(mat_to_shrink2)), ]
-    itermat_q_by_type[[gg]] <- mat_to_shrink2
-  }
-
-  ## create new matrix to store the p
   p_by_t_new <- matrix(nrow = n_features_per_cell, ncol = length(cell_type_set))
   colnames(p_by_t_new) <- cell_type_set
-  for (gg in cell_type_set) {
-    p_by_t_new_w0 <- itermat_p_by_type[[gg]][, dim(itermat_p_by_type[[gg]])[2]]
-    p_by_t_new_w0[p_by_t_new_w0 < 0.00005] <- 0.00005
-    p_by_t_new[, gg] <- p_by_t_new_w0
-  }
 
-  # save q information
-  q_vec_new <- vector(length = 0L)
-  for (gg in cell_type_set) {
-    q_vec_op <- itermat_q_by_type[[gg]][dim(itermat_q_by_type[[gg]])[1], ]
-    clabel <- colnames(r_by_c)[cell_type_labels == gg]
-    names(q_vec_op) <- clabel
-    # print(sum(is.na(q_vec_op)))
-    q_vec_new <- append(q_vec_new, q_vec_op)
-  }
-  q_vec_new <- q_vec_new[colnames(r_by_c)]
-  return(list(p_by_t_new = p_by_t_new, q_vec_new = q_vec_new))
-}
 
-#' Return
-#'
-#'
-#' @noRd
-get_r_by_ct_mat_pq_int <- function(cell_type_set,
-                                   r_by_c,
-                                   cell_type_labels,
-                                   n_features_per_cell,
-                                   p_acc = 0.0005,
-                                   q_acc = 0.0005,
-                                   n_max_iter = 300) {
-  ## save data to matrix
-  itermat_q_by_type <- rep(list(), length = length(cell_type_set))
-  names(itermat_q_by_type) <- cell_type_set
-  itermat_p_by_type <- rep(list(), length = length(cell_type_set))
-  names(itermat_p_by_type) <- cell_type_set
+  ## make the matrix binary
+  if (!is(r_by_c, "sparseMatrix")) {
+    as(r_by_c, "sparseMatrix")
+  }
+  r_by_c@x <- rep(r_by_c@x, length = length(r_by_c@x))
 
   ## for each cell type
   for (gg in cell_type_set) {
     r_by_c_sub <- r_by_c[, cell_type_labels == gg]
     n_cell_sub <- dim(r_by_c_sub)[2]
     # r_by_c_sub <- as.matrix(r_by_c_sub)
+    cell_names_sub <- colnames(r_by_c_sub)
     n_reads_in_cell <- colSums(r_by_c_sub)
     n_reads_in_region <- rowSums(r_by_c_sub)
 
@@ -97,17 +69,17 @@ get_r_by_ct_mat_pq_int <- function(cell_type_set,
     diff2 <- 1
     numiters <- 1
 
-    while ((diff1 > 0.0005 | diff2 > 0.0005) & numiters < n_max_iter) {
+    while ((diff1 > p_acc | diff2 > q_acc) & numiters < n_max_iter) {
       q0 <- itermat_q[numiters, ]
       p0 <- itermat_p[, numiters]
 
       ## First step -- estimate the missing rate from the open probabilities
       q0new <- n_reads_in_cell / sum(p0)
-      q0new[q0new > 1] <- 0.9995 ## make sure it does not exceed 1
+      q0new[q0new > 1] <- 0.999 ## make sure it does not exceed 1
 
       ## Second step
       p0new <- n_reads_in_region / sum(q0)
-      p0new[p0new > 1] <- 0.9995 ## make sure it does not exceed 1
+      p0new[p0new > 1] <- 0.999 ## make sure it does not exceed 1
 
       ## record the values
       numiters <- numiters + 1
@@ -115,17 +87,26 @@ get_r_by_ct_mat_pq_int <- function(cell_type_set,
       itermat_q[numiters, ] <- q0new
 
       # print(numiters)
-      diff1 <- sum(abs(p0new - itermat_p[, numiters - 1]) / abs(p0new), na.rm = T) / n_features_per_cell
-      diff2 <- sum(abs(q0new - itermat_q[numiters - 1, ]) / abs(q0new), na.rm = T) / n_cell_sub
+      diff1 <- sum(abs(itermat_p[, numiters] - itermat_p[, numiters - 1]) / abs(itermat_p[, numiters]), na.rm = T) / n_features_per_cell
+      diff2 <- sum(abs(itermat_q[numiters, ] - itermat_q[numiters - 1, ]) / abs(itermat_q[numiters, ]), na.rm = T) / n_cell_sub
     }
-    itermat_p <- itermat_p[, 1:numiters]
-    itermat_q <- itermat_q[1:numiters, ]
 
-    itermat_p_by_type[[gg]] <- itermat_p
-    itermat_q_by_type[[gg]] <- itermat_q
-    print(paste(gg, " completed"))
-    # print(paste('diff1 = ', diff1))
-    # print(paste('diff2 = ', diff2))
+    ## remove columns that contain na values
+    mat_to_shrink <- itermat_p
+    mat_to_shrink <- mat_to_shrink[, !is.na(colSums(mat_to_shrink))]
+    itermat_p_by_type[, gg] <- mat_to_shrink[, dim(mat_to_shrink)[2]]
+
+    mat_to_shrink2 <- itermat_q
+    mat_to_shrink2 <- mat_to_shrink2[!is.na(rowSums(mat_to_shrink2)), ]
+    itermat_q_by_type[cell_names_sub] <- mat_to_shrink2[dim(mat_to_shrink2)[1], ]
+
+    ## print progress
+    if(verbose){
+      print(paste(gg, " completed"))
+      print(paste("diff1 = ", diff1))
+      print(paste("diff2 = ", diff2))
+    }
+
   }
-  return(list(itermat_p_by_type = itermat_p_by_type, itermat_q_by_type = itermat_q_by_type))
+  return(list(p_by_t = itermat_p_by_type, q_vec = itermat_q_by_type))
 }
